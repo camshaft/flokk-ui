d9e50a5355a5921aeaf145c839e7c41690ccddb8
Cameron Bytheway <cameron@nujii.com>
2013-06-11 00:22:53 -0600

    Add auth stuff

--- app.js
+++ app.js
@@ -2,25 +2,40 @@
  * Module dependencies
  */
 var stack = require("simple-stack-common")
+  , envs = require("envs")
+  , auth = require("./lib/auth")
   , proxy = require("simple-http-proxy");
 
 /**
  * Defines
  */
-var API_URL = process.env.API_URL || "http://localhost:5001";
+var API_URL = envs("API_URL", "https://api.theflokk.com");
 
 /**
  * Expose the app
  */
 var app = module.exports = stack();
 
-app.configure(function() {
-  app.useBefore("router", "/public", "public", stack.middleware.static(__dirname+"/build", {maxAge: 3600}));
-  app.useBefore("router", "/partials", "partialNotFound", function(req, res) {
-    res.sendfile(__dirname+"/public/partials/404.nghtml");
-  });
+/**
+ * Serve the static assets
+ *
+ * @todo disable this stuff in prod
+ */
+app.useBefore("router", "/public", "public", stack.middleware.static(__dirname+"/build", {maxAge: 3600}));
+app.useBefore("router", "/partials", "partialNotFound", function(req, res) {
+  res.sendfile(__dirname+"/public/partials/404.nghtml");
 });
 
+/**
+ * Use flokk authentication
+ */
+app.useBefore("router", "/auth/login", "auth:login", auth);
+app.useBefore("router", "/auth/callback", "auth:callback", auth);
+app.useBefore("router", "/auth/logout", "auth:logout", auth.logout);
+
+/**
+ * Proxy the api
+ */
 app.configure("development", function() {
   app.useBefore("base", "/api", "api-proxy", proxy(API_URL, {xforward: true}));
 });
