d7309f56e9c70516d8d389f3287e38b66f5ee81e
Cameron Bytheway <cameron@nujii.com>
2013-06-18 01:13:10 -0600

    Pad the base64 encoded stuff

--- public/javascripts/lib/url-param.js
+++ public/javascripts/lib/url-param.js
@@ -4,16 +4,37 @@
 var base64decode = require("base64-decode")
   , base64encode = require("base64-encode");
 
-// TODO make it url safe
-
 exports.encode = function(url) {
   return url
     ? base64encode(url)
+        .replace(/=/g, '')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
     : '';
 };
 
 exports.decode = function(url) {
   return url
-    ? base64decode(url)
+    ? padString(base64decode(url))
+        .replace(/\-/, '+')
+        .replace(/_/, '/')
     : '';
+};
+
+exports.padString;
+
+function padString(string) {
+  var mod = string.length % 4;
+
+  // We don't require any padding
+  if(!mod) return string;
+
+  // See how much padding we need
+  var rem = 4-mod;
+
+  // Add it
+  while (rem--) {
+    string += "=";
+  }
+  return string;
 };
