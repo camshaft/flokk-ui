4682b6529170ab8a9949583eebf945349e0cbd58
Cameron Bytheway <cameron@nujii.com>
2013-06-18 01:13:53 -0600

    Remove the piecon loading for now

--- public/javascripts/lib/loading.js
+++ public/javascripts/lib/loading.js
@@ -1,22 +1,17 @@
 /**
  * Module dependencies
  */
-var Piecon = require("piecon")
-  , favicon = require("favicon");
 
-var piecon = new Piecon()
-  , loading = false;
+var loading = false;
 
 exports.start = function() {
-  // piecon.update(0);
   loading = true;
 };
 
 exports.update = function(progress) {
-  // if(loading) piecon.update(progress);
+
 };
 
 exports.end = function() {
-  // favicon.reset();
   loading = false;
 };
