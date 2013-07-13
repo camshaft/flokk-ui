825da851baff50cec47ad377caffac4bde948892
Cameron Bytheway <cameron@nujii.com>
2013-07-13 14:49:02 -0600

    Use the theme's layout

--- app.js
+++ app.js
@@ -2,10 +2,39 @@
  * Module dependencies
  */
 
-var stack = require("flokk-angular");
+var stack = require("flokk-angular")
+  , envs = require('envs');
 
 /**
  * Expose the app
  */
 
 var app = module.exports = stack();
+
+/**
+ * Configure the app
+ */
+
+app.locals({
+  ngapp: envs('APP_NAME', 'flokk'),
+  site: envs('SITE_URL', 'https://www.theflokk.com'),
+  title: envs('INDEX_TITLE', 'Home'),
+  description: envs('SITE_DESCRIPTION', ''),
+  env: {
+    BROWSER_ENV: envs('NODE_ENV', 'production')
+  }
+});
+
+// TODO pull these from the cdn
+
+app.locals({
+  styles: [
+    '/public/build.css'
+  ]
+});
+
+app.locals({
+  scripts: [
+    '/public/build.js'
+  ]
+});
