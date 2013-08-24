11eb7852f8368c3e3fe4331cd3a02c26953be662
Cameron Bytheway <cameron@theflokk.com>
2013-08-24 17:37:55 -0600

    Fix the stylus plugin

--- stylus.js
+++ stylus.js
@@ -2,8 +2,4 @@
  * Module dependencies
  */
 
-var stylus = module.exports = require('component-stylus-plugin');
-var nib = require('nib')();
-
-stylus.includeCSS = true;
-stylus.plugins.push(nib);
+module.exports = require('component-stylus');
